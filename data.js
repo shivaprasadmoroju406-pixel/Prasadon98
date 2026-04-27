cb521bf0802bcb1be9f0aa100a7e37295e9b30a5
shivaprasadmoroju406-pixel <shivaprasadmoroju406@gmail.com>
2026-04-27 15:24:18 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 const DATA = {
   stories: ["mookshitha.jpeg","kavitha.jpg","guntoju bramha chary.jpeg"],
   posts: ["mookshitha.jpeg","guntoju bramha chary.jpeg"],
-  reels: ["ammulu.mp4","ammulu.mp4"]
+  reels: ["prashu.mp4","ammulu.mp4"]
 };
